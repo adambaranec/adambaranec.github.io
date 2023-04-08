794656eb106c9b2f79887f679e4b8c7c237b7d54
Adam Baranec <abaranec@gmail.com>
2023-04-08 19:29:42 +0200

    some improvements

---
+++ arts/spiral.js
@@ -0,0 +1,48 @@
+let iterations = 100;
+let multiplier = 0;
+let radius = window.innerWidth/20;
+let look = 1.5;
+let densityX = 0;
+let densityY = 0;
+let red = 0.0;
+let green = 0.0;
+let blue = 0.0;
+let seconds = 0;
+
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+  noStroke();
+  noCursor();
+}
+
+function draw() {
+  background(red,green,blue);  
+  if (frameCount % 60 == 0){seconds += 1;}
+  if (seconds == 120){seconds = 0;}
+  if (seconds < 30){
+    densityX += 0.05; densityY += 0.05;
+  }
+  else if (seconds > 30 && seconds < 60){
+  fill(random(255), random(255), random(255));
+  }
+  else if (seconds > 60 && seconds < 120){
+   densityX -= 0.05; densityY -= 0.05;
+  }
+  if (minute() % 2 == 0){
+        for (let i = 1; i<=iterations; i++){
+          square((window.innerWidth/2-radius/2)+sin(i/look)*(multiplier+(i*densityX-0.02)),(window.innerHeight/2-radius/2)+cos(i/look)*(multiplier-(i*densityY-0.02)),radius);
+        }
+      }
+  if (minute() % 2 == 1){
+        for (let i = 1; i<=iterations; i++){
+          circle((window.innerWidth/2)+sin(i/look)*(multiplier+(i*densityX-0.02)),(window.innerHeight/2)+cos(i/look)*(multiplier-(i*densityY-0.02)),radius);
+        }
+  }
+  if (frameCount % 7200 == 0){
+  iterations = int(random(100)) + 20;
+  look = random(3) + 1.0;
+  red = random(80);
+  green = random(80);
+  blue = random(80);
+  }
+}
