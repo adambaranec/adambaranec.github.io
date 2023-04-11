04924e818d023aa6f3f1757c39b0567649959c4a
Adam Baranec <35851756+adambaranec@users.noreply.github.com>
2023-04-11 15:47:09 +0200

    Update spiral.js

--- arts/spiral.js
+++ arts/spiral.js
@@ -17,7 +17,7 @@ function setup() {
 function draw() {
   background(red,green,blue);  
   if (frameCount % 60 == 0){seconds += 1;}
-  if (seconds == 91){seconds = 1;}
+  if (seconds == 121){seconds = 1;}
   if (seconds <= 30){
     densityX += (30/1800); densityY += (30/1800);
   }
@@ -27,6 +27,9 @@ function draw() {
   else if (seconds > 60 && seconds < 91){
    densityX -= (30/1800); densityY -= (30/1800);
   }
+  else if (seconds > 90 && seconds < 121){
+  fill(random(255), random(255), random(255));
+  }  
   if (minute() % 2 == 0){
         for (let i = 1; i<=iterations; i++){
           square((window.innerWidth/2-radius/2)+sin(i/look)*(multiplier+(i*densityX-0.02)),(window.innerHeight/2-radius/2)+cos(i/look)*(multiplier-(i*densityY-0.02)),radius);
@@ -38,10 +41,16 @@ function draw() {
         }
   }
   if (frameCount % 7200 == 0){
-  iterations = int(random(100)) + 20;
+  iterations = int(random(20,120));
   look = random(3) + 1.0;
   red = random(80);
   green = random(80);
   blue = random(80);
+  }
+  if (frameCount % 21600 == 0){
+  iterations = 100;
+  look = 1.5; 
+  red = 0.0; green = 0.0; blue = 0.0;
+  fill(255,255,255);
   }
 }
